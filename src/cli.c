38f67dbf74d49edc2a27948ad2dc44c0ef60205f
= <bhavesh.goyal@students.iiit.ac.in>
2017-10-29 16:46:41 -0400

    Add ephemeral port and list mechanisms

--- src/cli.c
+++ src/cli.c
@@ -11,14 +11,53 @@ int main(int argc, char **argv){
     Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
     sockfd = Socket(AF_INET, SOCK_DGRAM, 0);
 
+//    Bind(sockfd, (SA *)&servaddr, sizeof(servaddr));
+    
     char sendline[MAXLINE], recvline[MAXLINE];
+    
+    fd_set readfs;
 
-    while(Fgets(sendline, MAXLINE, stdin) != NULL){
+    int maxfd = max(STDIN_FILENO, sockfd);
+    while(1){
+        FD_ZERO(&readfs);
+        FD_SET(STDIN_FILENO, &readfs);
+        FD_SET(sockfd, &readfs);
+        int status = select(maxfd+1, &readfs, NULL, NULL, NULL);
+        if (status < 0){
+            fprintf(stderr, "error: Can't select on sockets");
+            continue;
+        }
+        if (FD_ISSET(sockfd, &readfs)){
+            int n = Recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL);
+            recvline[n] = 0;
+            Fputs(recvline, stdout);
+        }
+        else if (FD_ISSET(STDIN_FILENO, &readfs)){
+            if(Fgets(sendline, MAXLINE, stdin) != NULL){
+                Sendto(sockfd, "REQ", strlen("REQ"), 0, (SA *)&servaddr, sizeof(servaddr));
+                char eph_port[MAXLINE];
+                // Also acts as ACK to command
+                int n = Recvfrom(sockfd, eph_port, MAXLINE, 0, NULL, NULL);
+                eph_port[n] = 0;
 
-        Sendto(sockfd, sendline, strlen(sendline), 0, (SA *)&servaddr, sizeof(servaddr));
-        int n = Recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL);
-        recvline[n] = 0;
-        Fputs(recvline, stdout);
+                struct sockaddr_in addrinfo;
+                int addr_size = sizeof(addrinfo);
+
+                getpeername(sockfd, (SA *)&addrinfo, &addr_size);
+                fprintf(stdout, "\nClient bound at ephemeral port at server %s:%s\n", inet_ntoa(addrinfo.sin_addr), eph_port);
+                fflush(stdout);
+                // Connect to ephemeral
+                struct sockaddr_in ephaddr;
+                int ephsock = Socket(AF_INET, SOCK_DGRAM, 0);
+                bzero(&ephaddr, sizeof(ephaddr));
+                ephaddr.sin_family = AF_INET;
+                ephaddr.sin_port = htons((int) strtol(eph_port, (char **)NULL, 10));
+                Inet_pton(AF_INET, argv[1], &ephaddr.sin_addr); 
+        
+                Sendto(sockfd, sendline, strlen(sendline), 0, (SA *)&ephaddr, sizeof(ephaddr));
+                 
+            }
+        }
     }
-    return 0;
+        return 0;
 }
--- src/srv.c
+++ src/srv.c
@@ -1,5 +1,5 @@
 #include "unp.h"
-
+#include <dirent.h>
 int main(int argc, char **argv){
     int sockfd;
     struct sockaddr_in servaddr, cliaddr;
@@ -19,11 +19,59 @@ int main(int argc, char **argv){
     char msg[MAXLINE];
 
     while(1){
+//        printf("Waiting to recv");
         int n = Recvfrom(sockfd, msg, MAXLINE, 0, (SA *)&cliaddr, &len);
+        int pid = fork();
+        if (pid == 0){
+            struct sockaddr_in ephaddr;
+            int eph_sock = Socket(AF_INET, SOCK_DGRAM, 0);
 
-        Sendto(sockfd, msg, n, 0, (SA *)&cliaddr, len);
-    
+            bzero(&ephaddr, sizeof(ephaddr));
+            ephaddr.sin_family = AF_INET;
+            ephaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+            Bind(eph_sock, (SA *)&ephaddr, sizeof(ephaddr));
+
+            struct sockaddr_in echoaddr;
+            int addr_size = sizeof(echoaddr);
+            getsockname(eph_sock, (SA *)&echoaddr, &addr_size);
+            printf("New UDP client: connection established at %s:%d\n", inet_ntoa(echoaddr.sin_addr), ntohs(echoaddr.sin_port));
+            fflush(stdout);
+            char ephport_res[MAXLINE];
+            sprintf(ephport_res, "%d", ntohs(echoaddr.sin_port));
+            Sendto(sockfd, ephport_res, strlen(ephport_res), 0, (SA *)&cliaddr, len);
+
+            char command[MAXLINE];
+            printf("nhgnnghnhlist issued");
+                fflush(stdout);
+            int n = Recvfrom(eph_sock, command, MAXLINE, 0, (SA *)&cliaddr, &len);
+
+            printf("list issued");
+                fflush(stdout);
+            if (strcmp(command, "list") >= 0){
+                printf("list issued");
+                fflush(stdout);
+                DIR *p_dir = opendir(".");
+                char resp[MAXLINE];
+                if (p_dir == NULL){
+                    strcpy(resp, "Can't open present directory for reading");
+                    fprintf(stderr, "%s", resp);
+                    Sendto(sockfd, resp, strlen(resp), 0, (SA *)&cliaddr, len);
+                }
+                else{
+                    struct dirent *pDirent;
+                    while ((pDirent = readdir(p_dir)) != NULL) {
+                        strcpy(resp, strcat(pDirent->d_name, "\n"));
+                        Sendto(sockfd, resp, strlen(resp), 0, (SA *)&cliaddr, len);
+                    }
+                    closedir (p_dir);
+                }
+            }
+            else
+                Sendto(sockfd, command, n, 0, (SA *)&cliaddr, len);
+        }
+
     }
 
-
+    return 0;
 }
