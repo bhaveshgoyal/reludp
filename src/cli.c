5bbab6383afdbe80474145c190971e91f62b84f6
= <bhavesh.goyal@students.iiit.ac.in>
2017-10-30 15:14:22 -0400

    Fix Ephermeral Port bug

--- src/cli.c
+++ src/cli.c
@@ -11,29 +11,46 @@ int main(int argc, char **argv){
     Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
     sockfd = Socket(AF_INET, SOCK_DGRAM, 0);
 
-    
+
     char sendline[MAXLINE], recvline[MAXLINE];
+
+    Sendto(sockfd, "REQ", strlen("REQ"), 0, (SA *)&servaddr, sizeof(servaddr));
+    char eph_port[MAXLINE];
     
+    // Also acts as ACK to command
+    // Waiting for ephemeral port
+    int n = Recvfrom(sockfd, eph_port, MAXLINE, 0, NULL, NULL);
+    eph_port[n] = 0;
+
+    struct sockaddr_in addrinfo;
+    int addr_size = sizeof(addrinfo);
+    getpeername(sockfd, (SA *)&addrinfo, &addr_size);
+    fprintf(stdout, "\nClient bound at ephemeral port at server %s:%s\n", inet_ntoa(addrinfo.sin_addr), eph_port);
+    fflush(stdout);
+    struct sockaddr_in ephaddr;
+    int ephsock = Socket(AF_INET, SOCK_DGRAM, 0);
+    bzero(&ephaddr, sizeof(ephaddr));
+    ephaddr.sin_family = AF_INET;
+    ephaddr.sin_port = htons((int) strtol(eph_port, (char **)NULL, 10));
+    Inet_pton(AF_INET, argv[1], &ephaddr.sin_addr); 
+
     fd_set readfs;
 
-    int maxfd = max(STDIN_FILENO, sockfd);
+    int maxfd = max(STDIN_FILENO, ephsock);
     
     printf("$> ");
     fflush(stdout);
-    while(1){
-        
-        
+    while(1){ 
         FD_ZERO(&readfs);
         FD_SET(STDIN_FILENO, &readfs);
-        FD_SET(sockfd, &readfs);
-        int maxfd = max(STDIN_FILENO, sockfd);
+        FD_SET(ephsock, &readfs);
         int status = select(maxfd+1, &readfs, NULL, NULL, NULL);
         if (status < 0){
             fprintf(stderr, "error: Can't select on sockets");
             continue;
         }
-        if (FD_ISSET(sockfd, &readfs)){
-            int n = Recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL);
+        if (FD_ISSET(ephsock, &readfs)){
+            int n = Recvfrom(ephsock, recvline, MAXLINE, 0, NULL, NULL);
             recvline[n] = 0;
             Fputs(recvline, stdout);
         }
@@ -44,26 +61,7 @@ int main(int argc, char **argv){
                     fflush(stdout);
                     continue;
                 }
-                Sendto(sockfd, "REQ", strlen("REQ"), 0, (SA *)&servaddr, sizeof(servaddr));
-                char eph_port[MAXLINE];
-                // Also acts as ACK to command
-                int n = Recvfrom(sockfd, eph_port, MAXLINE, 0, NULL, NULL);
-                eph_port[n] = 0;
-
-                struct sockaddr_in addrinfo;
-                int addr_size = sizeof(addrinfo);
-
-                getpeername(sockfd, (SA *)&addrinfo, &addr_size);
-                fprintf(stdout, "\nClient bound at ephemeral port at server %s:%s\n", inet_ntoa(addrinfo.sin_addr), eph_port);
-                fflush(stdout);
                 // Connect to ephemeral
-                struct sockaddr_in ephaddr;
-                int ephsock = Socket(AF_INET, SOCK_DGRAM, 0);
-                bzero(&ephaddr, sizeof(ephaddr));
-                ephaddr.sin_family = AF_INET;
-                ephaddr.sin_port = htons((int) strtol(eph_port, (char **)NULL, 10));
-                Inet_pton(AF_INET, argv[1], &ephaddr.sin_addr); 
-                sockfd = ephsock;
                 Sendto(ephsock, sendline, strlen(sendline), 0, (SA *)&ephaddr, sizeof(ephaddr));
                  
             }
--- src/srv.c
+++ src/srv.c
@@ -44,51 +44,53 @@ int main(int argc, char **argv){
 
             char command[MAXLINE];
             fflush(stdout);
-            int n = Recvfrom(eph_sock, command, MAXLINE, 0, (SA *)&cliaddr, &len);
+            while(1){
+                int n = Recvfrom(eph_sock, command, MAXLINE, 0, (SA *)&cliaddr, &len);
 
-            if (strcmp(command, "list") >= 0){
-      //          printf("list issued");
-     //           fflush(stdout);
-                DIR *p_dir = opendir(SHARED_PATH);
-                char resp[MAXLINE];
-                if (p_dir == NULL){
-                    strcpy(resp, "Can't open present directory for reading");
-                    fprintf(stderr, "%s", resp);
-                    Sendto(sockfd, resp, strlen(resp), 0, (SA *)&cliaddr, len);
-                }
-                else{
-                    struct dirent *pDirent;
-                    while ((pDirent = readdir(p_dir)) != NULL) {
-                        strcpy(resp, strcat(pDirent->d_name, "\n"));
-                        Sendto(sockfd, resp, strlen(resp), 0, (SA *)&cliaddr, len);
+                if (strcmp(command, "list") >= 0){
+                    //          printf("list issued");
+                    //           fflush(stdout);
+                    DIR *p_dir = opendir(SHARED_PATH);
+                    char resp[MAXLINE];
+                    if (p_dir == NULL){
+                        strcpy(resp, "Can't open present directory for reading");
+                        fprintf(stderr, "%s", resp);
+                        Sendto(eph_sock, resp, strlen(resp), 0, (SA *)&cliaddr, len);
                     }
-                    closedir (p_dir);
+                    else{
+                        struct dirent *pDirent;
+                        while ((pDirent = readdir(p_dir)) != NULL) {
+                            strcpy(resp, strcat(pDirent->d_name, "\n"));
+                            Sendto(eph_sock, resp, strlen(resp), 0, (SA *)&cliaddr, len);
+                        }
+                        closedir (p_dir);
+                    }
                 }
+                else if (strcmp(command, "download") >= 0){
+                    //           printf("download issued");
+                    //         fflush(stdout);
+                    char *d_args = strtok(command, " ");
+                    d_args = strtok(NULL, " \n");
+                    char path[MAXLINE];
+                    strcpy(path, SHARED_PATH);
+                    strcat(path, d_args);
+                    //            printf("Path: %s\n", path);
+                    //          fflush(stdout);
+                    int fd = open(path, O_RDONLY);
+                    if (fd < 0){
+                        char *msg = "Error Downloading file from server"; 
+                        Sendto(eph_sock, msg, strlen(msg), 0, (SA *) &cliaddr, len);
+                        continue;
+                    }
+                    char file_buf[MAXLINE];
+                    int n;
+                    while ((n = read(fd, file_buf, MAXLINE)) > 0) {
+                        sendto(eph_sock, file_buf, n, 0, (SA *) &cliaddr, len);
+                    }
+                }
+                else
+                    Sendto(eph_sock, command, n, 0, (SA *)&cliaddr, len);
             }
-            else if (strcmp(command, "download") >= 0){
-     //           printf("download issued");
-       //         fflush(stdout);
-                char *d_args = strtok(command, " ");
-                d_args = strtok(NULL, " \n");
-                char path[MAXLINE];
-                strcpy(path, SHARED_PATH);
-                strcat(path, d_args);
-    //            printf("Path: %s\n", path);
-      //          fflush(stdout);
-                int fd = open(path, O_RDONLY);
-                if (fd < 0){
-                    char *msg = "Error Downloading file from server"; 
-                    Sendto(sockfd, msg, strlen(msg), 0, (SA *) &cliaddr, len);
-                    continue;
-                }
-                char file_buf[MAXLINE];
-                int n;
-                while ((n = read(fd, file_buf, MAXLINE)) > 0) {
-                    sendto(sockfd, file_buf, n, 0, (SA *) &cliaddr, len);
-                }
-            }
-            else
-                Sendto(sockfd, command, n, 0, (SA *)&cliaddr, len);
         }
 
     }
