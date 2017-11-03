1c8e7fc2aa710d102c40ec2549edf08228bac77d
= <bhavesh.goyal@students.iiit.ac.in>
2017-11-03 14:00:16 -0400

    Establish Eph Handshake

--- src/cli.c
+++ src/cli.c
@@ -1,5 +1,43 @@
 #include "unp.h"
 
+int eph_port_recv = 0;
+
+// REQ ->; EPH_NUM <-; ACK ->
+int eph_cli_handshake(int sockfd, struct sockaddr_in *servaddr, char *serv_ip){
+
+    int servlen = sizeof(*servaddr);
+
+    char cmd[MAXLINE];
+    strcat(cmd, "REQ");
+    
+    Sendto(sockfd, cmd, strlen(cmd), 0, (SA *)servaddr, servlen);
+    char eph_port[MAXLINE];
+
+    //TODO Insert Timeout here 
+    int n = Recvfrom(sockfd, eph_port, MAXLINE, 0, NULL, NULL);
+    eph_port[n] = 0;
+
+    printf("port recieved: %s", eph_port);
+ 
+    int ephsock = Socket(AF_INET, SOCK_DGRAM, 0);
+    
+    bzero(servaddr, sizeof(*servaddr));
+    (*servaddr).sin_family = AF_INET;
+    (*servaddr).sin_port = htons((int) strtol(eph_port, (char **)NULL, 10));
+    Inet_pton(AF_INET, serv_ip, &servaddr->sin_addr); 
+    eph_port_recv = 1;
+
+    struct sockaddr_in addrinfo;
+    int addr_size = sizeof(addrinfo);
+    getpeername(sockfd, (SA *)&addrinfo, &addr_size);
+    fprintf(stdout, "\n\nClient bound at ephemeral port at server %s:%s\n\n", inet_ntoa(addrinfo.sin_addr), eph_port);
+    fflush(stdout);
+    
+    Sendto(sockfd, "ACK", strlen("ACK"), 0, (SA *)servaddr, servlen);
+    return ephsock;
+
+}
+
 int main(int argc, char **argv){
     int sockfd;
     struct sockaddr_in servaddr;
@@ -13,44 +51,26 @@ int main(int argc, char **argv){
 
 
     char sendline[MAXLINE], recvline[MAXLINE];
-
-    Sendto(sockfd, "REQ", strlen("REQ"), 0, (SA *)&servaddr, sizeof(servaddr));
-    char eph_port[MAXLINE];
-    
-    // Also acts as ACK to command
-    // Waiting for ephemeral port
-    int n = Recvfrom(sockfd, eph_port, MAXLINE, 0, NULL, NULL);
-    eph_port[n] = 0;
-
-    struct sockaddr_in addrinfo;
-    int addr_size = sizeof(addrinfo);
-    getpeername(sockfd, (SA *)&addrinfo, &addr_size);
-    fprintf(stdout, "\nClient bound at ephemeral port at server %s:%s\n", inet_ntoa(addrinfo.sin_addr), eph_port);
-    fflush(stdout);
-    struct sockaddr_in ephaddr;
-    int ephsock = Socket(AF_INET, SOCK_DGRAM, 0);
-    bzero(&ephaddr, sizeof(ephaddr));
-    ephaddr.sin_family = AF_INET;
-    ephaddr.sin_port = htons((int) strtol(eph_port, (char **)NULL, 10));
-    Inet_pton(AF_INET, argv[1], &ephaddr.sin_addr); 
+    int servlen = sizeof(servaddr);
 
     fd_set readfs;
 
-    int maxfd = max(STDIN_FILENO, ephsock);
+    int maxfd = max(STDIN_FILENO, sockfd);
     
     printf("$> ");
     fflush(stdout);
     while(1){ 
         FD_ZERO(&readfs);
         FD_SET(STDIN_FILENO, &readfs);
-        FD_SET(ephsock, &readfs);
+        FD_SET(sockfd, &readfs);
+        int maxfd = max(STDIN_FILENO, sockfd);
         int status = select(maxfd+1, &readfs, NULL, NULL, NULL);
         if (status < 0){
             fprintf(stderr, "error: Can't select on sockets");
             continue;
         }
-        if (FD_ISSET(ephsock, &readfs)){
-            int n = Recvfrom(ephsock, recvline, MAXLINE, 0, NULL, NULL);
+        if (FD_ISSET(sockfd, &readfs)){
+            int n = Recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL);
             recvline[n] = 0;
             Fputs(recvline, stdout);
         }
@@ -61,9 +81,17 @@ int main(int argc, char **argv){
                     fflush(stdout);
                     continue;
                 }
-                // Connect to ephemeral
-                Sendto(ephsock, sendline, strlen(sendline), 0, (SA *)&ephaddr, sizeof(ephaddr));
-                 
+                if (eph_port_recv == 0){
+                    if ((sockfd = eph_cli_handshake(sockfd, &servaddr , argv[1])) > 0){
+                        eph_port_recv = 1;
+                    }
+                    else{
+                        fprintf(stderr, "Something went wrong while doing handshake. Abort\n");
+                        exit(0);
+                    }
+                }
+                Sendto(sockfd, sendline, strlen(sendline), 0, (SA *)&servaddr, sizeof(servaddr));
+
             }
         }
     }
--- src/srv.c
+++ src/srv.c
@@ -1,12 +1,65 @@
 #include "unp.h"
+#include "unpifi.h"
 #include <dirent.h>
 #define SHARED_PATH "./"
+
+int eph_serv_handshake(struct sockaddr_in *cliaddr){
+
+    int len = sizeof(*cliaddr);
+    struct sockaddr_in ephaddr;
+    int eph_sock = Socket(AF_INET, SOCK_DGRAM, 0);
+
+    bzero(&ephaddr, sizeof(ephaddr));
+    ephaddr.sin_family = AF_INET;
+    ephaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    ephaddr.sin_port = htons(0);
+
+    Bind(eph_sock, (SA *)&ephaddr, sizeof(ephaddr));
+
+
+    struct sockaddr_in echoaddr;
+    int addr_size = sizeof(echoaddr);
+    getsockname(eph_sock, (SA *)&echoaddr, &addr_size);
+    printf("New UDP client Request: Binding to server at %s:%d\n", inet_ntoa(echoaddr.sin_addr), ntohs(echoaddr.sin_port));
+    fflush(stdout);
+    
+    char ephport_res[MAXLINE];
+    sprintf(ephport_res, "%d", ntohs(echoaddr.sin_port));
+
+    //#TODO Send two copies on timeout
+    Sendto(eph_sock, ephport_res, strlen(ephport_res), 0, (SA *)cliaddr, len);
+
+    char msg[MAXLINE];
+    Recvfrom(eph_sock, msg, MAXLINE, 0, (SA *)cliaddr, &len);
+ //   printf("recieved ack: %s", msg);
+    
+    if (strstr(msg, "ACK"))
+        return eph_sock;
+    return -1;
+
+}
 int main(int argc, char **argv){
     int sockfd;
-    struct sockaddr_in servaddr, cliaddr;
+    const int on = 1;
+    struct sockaddr_in *servaddr, cliaddr;
+    struct ifi_info *ifi, *ifihead;
 
+ //   struct sockaddr_in *sa;
+//    ifihead = Get_ifi_info(AF_INET, 1);
+    for (ifihead = ifi = Get_ifi_info(AF_INET, 1); ifi != NULL;ifi=ifi->ifi_next){
     sockfd = Socket(AF_INET, SOCK_DGRAM, 0);
+    
+    Setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+    
+    servaddr = (struct sockaddr_in *) ifi->ifi_addr;
+    servaddr->sin_family = AF_INET;
+    servaddr->sin_port = htons(SERV_PORT);
+    Bind(sockfd, (SA *) servaddr, sizeof(*servaddr));
 
+    printf("Server bound at interface %s\n", Sock_ntop((SA *) servaddr, sizeof(*servaddr)));
+    
+    }
+    /*
     bzero(&servaddr, sizeof(servaddr));
 
     servaddr.sin_family = AF_INET;
@@ -14,7 +67,7 @@ int main(int argc, char **argv){
     servaddr.sin_port = htons(SERV_PORT);
 
     Bind(sockfd, (SA *) &servaddr, sizeof(servaddr));
-    
+    */
     int len = sizeof(cliaddr);
 
     char msg[MAXLINE];
@@ -24,32 +77,26 @@ int main(int argc, char **argv){
         int n = Recvfrom(sockfd, msg, MAXLINE, 0, (SA *)&cliaddr, &len);
         int pid = fork();
         if (pid == 0){
-            struct sockaddr_in ephaddr;
-            int eph_sock = Socket(AF_INET, SOCK_DGRAM, 0);
-
-            bzero(&ephaddr, sizeof(ephaddr));
-            ephaddr.sin_family = AF_INET;
-            ephaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-            Bind(eph_sock, (SA *)&ephaddr, sizeof(ephaddr));
-
-            struct sockaddr_in echoaddr;
-            int addr_size = sizeof(echoaddr);
-            getsockname(eph_sock, (SA *)&echoaddr, &addr_size);
-            printf("New UDP client: connection established at %s:%d\n", inet_ntoa(echoaddr.sin_addr), ntohs(echoaddr.sin_port));
-            fflush(stdout);
-            char ephport_res[MAXLINE];
-            sprintf(ephport_res, "%d", ntohs(echoaddr.sin_port));
-            Sendto(sockfd, ephport_res, strlen(ephport_res), 0, (SA *)&cliaddr, len);
-
-            char command[MAXLINE];
-            fflush(stdout);
+            char command[MAXLINE] = {0};
+            printf("Recieved: %s\n", msg);
+            int eph_sock = eph_serv_handshake(&cliaddr);
+            if (eph_sock > 0){
+                struct sockaddr_in echoaddr;
+                int addr_size = sizeof(echoaddr);
+                getsockname(eph_sock, (SA *)&echoaddr, &addr_size);
+                printf("Handshake established at %s:%d\n", inet_ntoa(echoaddr.sin_addr), ntohs(echoaddr.sin_port));
+                fflush(stdout);
+            }
+            else{
+                fprintf(stderr, "Could not establish UDP ephemeral port handshake. Abort\n");
+                exit(0);
+            }
             while(1){
-                int n = Recvfrom(eph_sock, command, MAXLINE, 0, (SA *)&cliaddr, &len);
+                Recvfrom(eph_sock, command, MAXLINE, 0, (SA *)&cliaddr, &len);
 
-                if (strcmp(command, "list") >= 0){
-                    //          printf("list issued");
-                    //           fflush(stdout);
+                if (strstr(command, "list")){
+                    printf("list issued");
+                    fflush(stdout);
                     DIR *p_dir = opendir(SHARED_PATH);
                     char resp[MAXLINE];
                     if (p_dir == NULL){
@@ -60,15 +107,16 @@ int main(int argc, char **argv){
                     else{
                         struct dirent *pDirent;
                         while ((pDirent = readdir(p_dir)) != NULL) {
-                            strcpy(resp, strcat(pDirent->d_name, "\n"));
-                            Sendto(eph_sock, resp, strlen(resp), 0, (SA *)&cliaddr, len);
+                            strcat(resp, strcat(pDirent->d_name, "\n"));
                         }
+                        Sendto(eph_sock, resp, strlen(resp), 0, (SA *)&cliaddr, len);
                         closedir (p_dir);
                     }
                 }
-                else if (strcmp(command, "download") >= 0){
-                    //           printf("download issued");
-                    //         fflush(stdout);
+                else if (strstr(command, "download")){
+                    printf("download issued");
+                    printf("%s", command);
+                    fflush(stdout);
                     char *d_args = strtok(command, " ");
                     d_args = strtok(NULL, " \n");
                     char path[MAXLINE];
@@ -90,6 +138,7 @@ int main(int argc, char **argv){
                 }
                 else
                     Sendto(eph_sock, command, n, 0, (SA *)&cliaddr, len);
+                memset(command, 0, sizeof(command));
             }
         }
 
